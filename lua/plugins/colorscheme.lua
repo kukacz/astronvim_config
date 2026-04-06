13b608dfccb4bc34b2bf07be60e2471a5861eee2
Lukáš Kubín <lukas.kubin@eset.com>
2026-04-06 15:38:36 +0200

    config(colorscheme): add alabaster colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "AstroNvim/astroui",
+    ---@type AstroUIOpts
+    opts = {
+      colorscheme = "alabaster",
+    },
+  },
+  {
+    "mcncl/alabaster.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+}
